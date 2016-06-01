3d32e6c9a2eae75af9783498a09e34ddfe0cd158
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-06-01 13:24:44 +0200

    Working version with no generics

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,123 @@
+use std::io;
 mod redux;
-use redux::{ Store, Action };
+use redux::{ Store, Action, State, Todo, VisibilityFilter };
 use redux::Action::*;
+use redux::TodoAction::*;
+use redux::VisibilityFilter::*;
 
-fn reducer(state: i32, action: Action) -> i32 {
-    match action {
-        Increment => state + 1,
-        Decrement => state - 1,
+fn reducer(state: State, action: Action) -> State {
+    // Always return a new state
+    State {
+        todos: todo_reducer(&state.todos, &action),
+        visibility_filter: visibility_reducer(&state.visibility_filter, &action),
     }
 }
 
-fn render(state: i32) {
-    println!("State: {}", state);
+fn todo_reducer(state: &Vec<Todo>, action: &Action) -> Vec<Todo> {
+    let mut new_state: Vec<Todo> = state.clone();
+
+    // First we make sure it's a `Todos` action, otherwise return clone of incoming state
+    match *action {
+        Todos(ref todo_action) => match *todo_action {
+            // Pretty simple from here on, check the type of Todos enum type
+            // If Add push a new item, and if `Toggle` or `Remove` use our get_mut_todo
+            // helper function and then change a property on the todo
+            Add(ref title) => {
+                let new_id = new_state.len() as i16 + 1;
+                new_state.push(Todo::new(new_id, title.to_string()))
+            },
+            Toggle(todo_id) => {
+                if let Some(todo) = get_mut_todo(&mut new_state, todo_id) {
+                    if todo.completed { todo.completed = false; } else { todo.completed = true; }
+                }
+            },
+            Remove(todo_id) => {
+                if let Some(todo) = get_mut_todo(&mut new_state, todo_id) {
+                    todo.deleted = true;
+                }
+            },
+        },
+        // If it's not a Todos action change nothing
+        _ => (),
+    }
+    return new_state;
+}
+
+fn visibility_reducer(state: &VisibilityFilter, action: &Action) -> VisibilityFilter {
+    match *action {
+        Visibility(ref vis_action) => vis_action.clone(),
+        _ => state.clone(),
+    }
+}
+
+fn get_mut_todo(todos: &mut Vec<Todo>, todo_id: i16) -> Option<&mut Todo> {
+    todos.iter_mut().find(|todo|todo.id == todo_id)
+}
+
+fn print_todo(todo: &Todo) {
+    let done = if todo.completed { "✔" } else { " " };
+    println!("[{}] {} {}", done, todo.id, todo.title);
+}
+
+fn print_instructions() {
+    println!("\nAvailable commands: \nadd [text] - toggle [id] - remove [id]\nshow [all|active|completed]");
+}
+
+fn invalid_command(command: &str) {
+    println!("Invalid command: {}", command);
+}
+
+fn render(state: State) {
+    let visibility = &state.visibility_filter;
+    println!("\n\nTodo List:\n-------------------");
+    for i in 0..state.todos.len() {
+        let todo = &state.todos[i];
+        if !todo.deleted {
+            match *visibility {
+                ShowAll => print_todo(&todo),
+                ShowCompleted => if todo.completed { print_todo(&todo) },
+                ShowActive => if !todo.completed { print_todo(&todo) },
+            }
+        }
+    }
+    println!("-------------------\nVisibility filter:  {:?}", visibility);
+    print_instructions();
 }
 
 fn main() {
     let mut store = Store::create_store(reducer);
     store.subscribe(render);
 
-    store.dispatch(Increment);
-    store.dispatch(Decrement);
-    store.dispatch(Increment);
-    store.dispatch(Decrement);
-    store.dispatch(Increment);
-    store.dispatch(Decrement);
+    print_instructions();
+    loop {
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("failed to read line");
+        let command_parts: Vec<&str> = command.split_whitespace().collect();
+
+        match command_parts.len() {
+            0 => invalid_command(&command),
+            _ => {
+                match command_parts[0] {
+                    // Since we prepared so well we just need to call dispatch on our store
+                    // With the right action
+                    "add" => store.dispatch( Todos(Add( command_parts[1..].join(" ").to_string() ))),
+                    "remove" => if let Ok(num) = command_parts[1].parse::<i16>() {
+                        store.dispatch( Todos(Remove(num)));
+                    },
+                    "toggle" => if let Ok(num) = command_parts[1].parse::<i16>() {
+                        store.dispatch( Todos(Toggle(num)));
+                    },
+                    "show" => match command_parts[1] {
+                        "all" => store.dispatch( Visibility(ShowAll) ),
+                        "active" => store.dispatch( Visibility(ShowActive) ),
+                        "completed" => store.dispatch( Visibility(ShowCompleted) ),
+                        _ => invalid_command(&command)
+                    },
+                    _ => invalid_command(&command),
+                }
+            },
+        }
+    }
 }
