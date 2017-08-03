f718cef8de3be05a45995a8506ac330f934969d3
marsha88 <marsha88@purdue.edu>
2017-08-02 20:41:46 -0400

    Added RefCell type to Store. Reducers are currently implemented to accept a RefMut<T> type, but this needs to change in the future. Ideally, the reducers should return a new State type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use std::clone::Clone;
 use std::cell::RefCell;
+use std::cell::Ref;
+use std::cell::RefMut;
 
 #[allow(dead_code)]
 pub struct Store<T: Clone, U> {
     state: RefCell<T>,
-    listeners: RefCell<Vec<fn(&T)>>,
-    middlewares: RefCell<Vec<fn(&Store<T, U>, &U)>>,
-    reducer: fn(&T,&U) -> T,
+    listeners: RefCell<Vec<fn(Ref<T>)>>,
+    middlewares: RefCell<Vec<fn(&Store<T,U>, &U)>>,
+    reducer: fn(RefMut<T>,&U),
 }
 
 #[allow(dead_code)]
 impl<T: Clone, U> Store<T, U> {
-    pub fn create_store(reducer: fn(&T, &U) -> T, initial_state: T) -> Store<T, U> {
+    pub fn create_store(reducer: fn(RefMut<T>, &U), initial_state: T) -> Store<T, U> {
         Store {
             state: RefCell::new(initial_state),
             listeners: RefCell::new(Vec::new()),
@@ -19,24 +21,29 @@ impl<T: Clone, U> Store<T, U> {
             reducer: reducer
         }
     }
-    pub fn subscribe(&self, listener: fn(&T)) -> &Store<T, U> {
+    pub fn subscribe(&self, listener: fn(Ref<T>)) -> &Store<T, U> {
         self.listeners.borrow_mut().push(listener);
         self
     }
 
-    pub fn get_state(&self) -> &T {
+    pub fn apply_middleware(&self, middleware:fn(&Store<T,U>, &U)) -> &Store<T,U> {
+        self.middlewares.borrow_mut().push(middleware);
+        self
+    }
+
+    pub fn get_state(&self) -> Ref<T> {
         self.state.borrow()
     }
 
     pub fn dispatch(&self, action:U) {
-        self.state = (self.reducer)(&self.state.borrow(), &action);
+        (self.reducer)(self.state.borrow_mut(), &action);
 
         for middleware in self.middlewares.borrow().iter(){
-            middleware(&self.state.borrow(), &action);
+            middleware(self, &action);
         }
 
         for listener in self.listeners.borrow().iter() {
-            listener(&self.state.borrow())
+            listener(self.state.borrow())
         }
     }
 }
